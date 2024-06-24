efc41b7752479e82876f2ec0f3715ac0900ffe57
Aman-Pandey923 <amanp22@iitk.ac.in>
2024-06-24 16:41:57 +0530

    Install prettier and fix format

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,32 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "LeetCode Board",
   description: "Online WhiteBoarding for LeetCode problems.",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider 
+        <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-        > 
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import { Button } from "@/components/ui/button";
-import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/mode-toggle"
 
 export default function Home() {
   return (
-  <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
-    <div className="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-      <div className="flex space-x-4">
-        <Button> Hello </Button>
-        <ModeToggle />
+    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
+      <div className="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+        <div className="flex space-x-4">
+          <Button> Hello </Button>
+          <ModeToggle />
+        </div>
       </div>
     </div>
-  </div>
-  );
+  )
 }
