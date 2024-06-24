192308f92fff0479db067dbe9fc5e2c9f728c910
Aman-Pandey923 <amanp22@iitk.ac.in>
2024-06-24 17:02:07 +0530

    Install prettier and fix format Added Site-Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import { SiteHeader } from "@/components/site-header"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SiteHeader />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
