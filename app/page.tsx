c89d4c89f3bb1421ab8bd14f50b0204389143d53
Aman-Pandey923 <amanp22@iitk.ac.in>
2024-07-04 00:47:09 +0530

    implemented redirect to signin for null user and logout

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,47 @@
-import { Button } from "@/components/ui/button"
-import { ModeToggle } from "@/components/mode-toggle"
+'use client'
+
+import { useEffect, useState } from 'react';
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/lib/firebase/crud';
+import { useRouter } from "next/navigation";
+import { signOut } from "firebase/auth";
 
 export default function Home() {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  const [userSession, setUserSession] = useState<string | null>(null);
+
+  useEffect(() => {
+    const session = sessionStorage.getItem('user');
+    setUserSession(session);
+
+    if (!user && !session) {
+      router.push('/sign-in');
+    }
+  }, [user, router]);
+
+  useEffect(() => {
+    if (!user && userSession === null) {
+      router.push('/sign-in');
+    }
+  }, [user, userSession, router]);
+
+  console.log({ user });
+
   return (
     <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
       <div className="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
         <div className="flex space-x-4">
+          <Button onClick={() => {
+            signOut(auth);
+            sessionStorage.removeItem('user');
+          }}> Log Out </Button>
           <Button> Hello </Button>
           <ModeToggle />
         </div>
       </div>
     </div>
-  )
+  );
 }
