d4f8a6fab3d16ca80c24f66bb4611beb33b9f378
Aman-Pandey923 <amanp22@iitk.ac.in>
2024-07-04 19:11:19 +0530

    Modified Home page

--- app/page.tsx
+++ app/page.tsx
@@ -32,18 +32,32 @@ export default function Home() {
 
   return (
     <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
-      <div className="border rounded-lg relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-        <div className="flex space-x-4">
-          <Button> Hello </Button>
+      <div className="border rounded-lg relative px-10 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-2xl sm:rounded-lg sm:px-10">
+        <div className="mb-8">
+          <h1 className="text-center text-3xl font-bold">Welcome to CodeBoard</h1>
+          <p className="mt-4 text-justify leading-normal">
+            CodeBoard is the ultimate tool for enhancing your LeetCode experience. 
+            Designed for whiteboard interviews, CodeBoard bridges the gap by offering a seamless whiteboard integration for each LeetCode problem. Brainstorm, visualize, and save your problem-solving ideas directly on an interactive whiteboard. Log in to save your notes and revisit them anytime.
+          </p>
+        </div>
+        <div className="flex justify-center">
+          <Button
+            onClick={() => {
+              router.push('/problems');
+            }}
+            className="px-6 py-2 text-white bg-indigo-600 hover:bg-indigo-700 rounded-lg shadow-md transition duration-150 ease-in-out transform hover:scale-105 focus:outline-none"
+          >
+            Get Started
+          </Button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export function LogOutButton() {
   return (
-    <Button className="px-6 py-2 bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 rounded-lg shadow-md transition duration-150 ease-in-out transform hover:scale-105"
+    <Button className="px-6 py-2 text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 rounded-lg shadow-md transition duration-150 ease-in-out transform hover:scale-105"
       onClick={() => {
         signOut(auth)
         sessionStorage.removeItem("user")
